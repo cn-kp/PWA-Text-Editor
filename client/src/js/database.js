25ea184a3ebcbbb2805e8da1a89dba09b0fcd164
kevin peng <peng123kevin@gmail.com>
2022-04-19 18:34:01 +1000

    resolved service worker issue

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,26 +19,29 @@ export const putDb = async (content) => {
   // makes new transaction and specify the data used
   const tx = jateDb.transaction("jate", "readwrite");
   // creates the obj store
-  const objStore = tx.objectStore("jate");
+  const store = tx.objectStore("jate");
   // uses put to update the database
-  const req = objStore.put({ id: id, value: value });
-  const res = await req;
+  const request = store.put({ id:1, value: content });
+  const result = await request;
   // console log to confirm the update
-  console.log("data updated successfully", res);
+  console.log("data updated successfully", result.value);
 };
 // GET all function
 export const getDb = async () => {
   // connects to the jate DB and the version we are using
   const jateDb = await openDB("jate", 1);
   // makes new transaction and specify the data used
-  const tx = jateDb.transaction("jate", "readwrite");
+  const tx = jateDb.transaction("jate", "readonly");
   // creates the obj store
-  const objStore = tx.objectStore("jate");
+  const store = tx.objectStore("jate");
   // use get to fetch data from DB
-  const req = objStore.getAll();
-  const res = await req;
+  const request = store.get(1);
+  const result = await request;
   // console log to confirm data was fetched
-  console.log("data retrieved successfully", res);
-  return res;
+  result
+  ?
+  console.log("data retrieved successfully", result.value)
+  :console.log("nod data");
+  return result?.value;
 };
 initdb();
